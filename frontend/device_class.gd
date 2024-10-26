4bd633d4087d1fd8079387b259261e76cf18023b
kinn-edendev <nperematko@gmail.com>
2024-10-25 17:18:12 -0700

    feat: connected device JSON with internal dictionary schema, removed previous dictionary gdscript

--- frontend/device_class.gd
+++ frontend/device_class.gd
@@ -6,10 +6,12 @@ var name: String
 var coordinates: Vector2i
 var atlas_coordinates: Vector2i
 var status: String
+var user: String
 
 # Constructor for the TileDesktop object
-func _init(name: String, coordinates: Vector2i, atlas_coordinates: Vector2i, status: String):
+func _init(name: String, coordinates: Vector2i, atlas_coordinates: Vector2i, status: String, user: String):
 	self.name = name
 	self.coordinates = coordinates
 	self.atlas_coordinates = atlas_coordinates
 	self.status = status
+	self.user = user
--- frontend/scripts/devices.gd
+++ frontend/scripts/devices.gd
@@ -8,16 +8,18 @@ var device_coordinates = {}
 var devices = populate_desktops()
 
 func _ready():
-	device_coordinates = load_json_file(NamesDictionary)
+	device_coordinates = convert_keys_to_vector2i(load_json_file(NamesDictionary))
 	assign_names_to_desktops(devices)
+	for device in devices:
+		print(get_device_info(device))
 
 func assign_names_to_desktops(devices: Array):
 	# Called by ready()
 	for device in devices:
 		var coord = device.coordinates
-#		if coord in NamesDictionary:
+		if coord in device_coordinates.keys():
 			# Assign the name from the dictionary
-#			desktop.name = NamesDictionary.name_dictionary[coord]
+			device.name = device_coordinates[coord]
 
 func get_status_from_atlas(atlas_coords: Vector2i) -> String:
 	# Called by populate_desktops(), get_all_tile_coordinates()
@@ -35,7 +37,7 @@ func populate_desktops() -> Array:
 	for coord in tile_coords:
 		var atlas_coords = get_cell_atlas_coords(Vector2i(coord))
 		var status = get_status_from_atlas(atlas_coords)
-		var desktop = Device.new("Default", coord, atlas_coords, status)
+		var desktop = Device.new("Default", coord, atlas_coords, status, "Guest")
 		desktops.append(desktop)
 		
 	return desktops
@@ -52,7 +54,12 @@ func get_all_tile_coordinates():
 		
 	return tile_coordinates
 
+func get_device_info(device: Device):
+	# Called by ready()
+	return [device.name, device.status, device.user, device.coordinates, device.atlas_coordinates]
+
 func load_json_file(filePath: String):
+	# Called by ready()
 	if FileAccess.file_exists(filePath):
 		var dataFile = FileAccess.open(filePath, FileAccess.READ)
 		var parsedResult = JSON.parse_string(dataFile.get_as_text())
@@ -62,3 +69,24 @@ func load_json_file(filePath: String):
 			print("Error Reading File")
 	else:
 		print("File doesn't exist!")
+
+func convert_keys_to_vector2i(dictionary: Dictionary) -> Dictionary:
+	# Called by ready()
+	var result = {}
+	for key in dictionary.keys():
+		var coord = string_to_vector2(key)
+		var vector = Vector2i(coord)
+		result[vector] = dictionary[key]
+	return result
+	
+static func string_to_vector2(string := "") -> Vector2:
+	# Called by convert_keys_to_vector2i()
+	if string:
+		var new_string: String = string
+		new_string = new_string.erase(0, 1)
+		new_string = new_string.erase(new_string.length() - 1, 1)
+		var array: Array = new_string.split(", ")
+
+		return Vector2(int(array[0]), int(array[1]))
+
+	return Vector2.ZERO
