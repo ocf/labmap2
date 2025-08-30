d8425d7121587188faa3734716b257ed4748c50f
sophiebsw <sophiebsw@gmail.com>
2025-08-30 16:36:25 -0700

    add .transpire.py

---
+++ .transpire.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+from transpire.resources import Deployment, Ingress, Secret, Service
+from transpire.types import Image
+from transpire.utils import get_image_tag, get_revision
+
+name = "labmap2"
+auto_sync = True
+
+
+def images():
+    yield Image(name="backend", path=Path("/backend/data_generator_server/"), registry="ghcr")
+    yield Image(name="frontend", path=Path("/frontend/labmap2_server/"), registry="ghcr")
+
+
+def add_labmap_common(dep):
+    dep.obj.spec.template.spec.dns_policy = "ClusterFirst"
+    dep.obj.spec.template.spec.dns_config = {"searches": ["ocf.berkeley.edu"]}
+
+    dep.pod_spec().with_secret_env("labmap2")
+
+    dep.obj.spec.template.spec.containers[0].env = [
+        {"name": "GENERATE_URI", "value": "labmap2-backend/generate"},
+        {"name": "GET_URI", "value": "labmap2-backend/get"},
+    ]
+
+
+def add_probes(dep, path="/"):
+    dep.obj.spec.template.spec.containers[0].readiness_probe = {
+        "httpGet": {
+            "path": path,
+            "port": 8000,
+            "httpHeaders": [{"name": "Host", "value": "www.ocf.berkeley.edu"}],
+        },
+        "initialDelaySeconds": 5,
+        "periodSeconds": 5,
+    }
+
+    dep.obj.spec.template.spec.containers[0].liveness_probe = {
+        "httpGet": {
+            "path": path,
+            "port": 8000,
+            "httpHeaders": [{"name": "Host", "value": "www.ocf.berkeley.edu"}],
+        },
+        "initialDelaySeconds": 10,
+        "timeoutSeconds": 3,
+        "failureThreshold": 6,
+    }
+
+
+def objects():
+    yield Secret(
+        name="labmap2",
+        string_data={
+            "PROMETHEUS_PASSWORD": "",
+        },
+    ).build()
+
+    dep_backend = Deployment(
+        name="labmap-backend",
+        image=get_image_tag("backend"),
+        ports=[8081],
+    )
+    add_labmap_common(dep_backend)
+    add_probes(dep_backend)
+    yield dep_backend.build()
+
+    dep_frontend = Deployment(
+        name="labmap-frontend",
+        image=get_image_tag("frontend"),
+        ports=[8080],
+    )
+    add_labmap_common(dep_frontend)
+    yield dep_frontend.build()
+
+    svc_backend = Service(
+        name="labmap2-backend",
+        selector=dep_backend.get_selector(),
+        port_on_pod=8081,
+        port_on_svc=80,
+    )
+    yield svc_backend.build()
+
+    svc_frontend = Service(
+        name="labmap2-frontend",
+        selector=dep_frontend.get_selector(),
+        port_on_pod=8080,
+        port_on_svc=80,
+    )
+    yield svc_frontend.build()
+
+    ing_frontend = Ingress.from_svc(
+        svc=svc_frontend,
+        host="labmap.ocf.berkeley.edu",
+        path_prefix="/",
+    )
+    yield ing_frontend.build()
