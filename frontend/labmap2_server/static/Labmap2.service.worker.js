38b0a4a8c8d8f96e1026765a337926e8edab1ac5
kinn-edendev <kinn.edendev@gmail.com>
2025-08-30 21:49:07 -0700

    fix: no longer accepting gzip
    Signed-off-by: kinn-edendev <kinn.edendev@gmail.com>

--- frontend/labmap2_server/static/Labmap2.service.worker.js
+++ frontend/labmap2_server/static/Labmap2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756614929|17328653';
+const CACHE_VERSION = '1756615699|173941230';
 /** @type {string} */
 const CACHE_PREFIX = 'Labmap2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
