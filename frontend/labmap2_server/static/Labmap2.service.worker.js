4a589290cbdbe583b3d0364255fe22b8b4914696
kinn-edendev <kinn.edendev@gmail.com>
2025-09-02 20:46:53 -0700

    fix: build commit and refresh fix

--- frontend/labmap2_server/static/Labmap2.service.worker.js
+++ frontend/labmap2_server/static/Labmap2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756862485|35018516';
+const CACHE_VERSION = '1756871002|142573958';
 /** @type {string} */
 const CACHE_PREFIX = 'Labmap2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
