c7661b882263cf8c67ff6ff7ed07d0daceae37c9
kinn-edendev <kinn.edendev@gmail.com>
2025-09-10 15:53:14 -0700

    fix: away with flipflop and memory leaks, better http request handling and no more children.

--- frontend/labmap2/scripts/http_request_node.gd
+++ frontend/labmap2/scripts/http_request_node.gd
@@ -3,91 +3,61 @@ This script manages the requests and data coming from the data_generator_server.
 It sends two requests to the endpoints of the data generator, one receiving processed
 data from Prometheus and another receiving data from the user-controlled API.
 They are result_from_generate and result_from_get respectively.
-In order to get these seperate requests, meta data is used to organize the results.
-Some really bad code is used to query the http_request children of their data, but
-if you can overlook that, it works fine.
 '''
 
 extends HTTPRequest
 
+@onready var http_generate := HTTPRequest.new()
+@onready var http_get := HTTPRequest.new()
+
 @onready var result_from_generate: Dictionary = {}
 @onready var result_from_get: Dictionary = {}
-
-# This was part of a dynamic endpoint structure using an .env
-# We ended up hard coding the endpoints instead.
-#var GENERATE;
-#var GET;
-var env_vars = {};
 
 const GENERATE = "https://labmap.ocf.berkeley.edu/api/generate"
 const GET = "https://labmap.ocf.berkeley.edu/api/get"
 
 signal result_ready(endpoint: String)
 
-var flipflop = 1 # don't ask
-
 func _ready():
-#	load_env("res://.env")
-#	GENERATE = env_vars["GENERATE_URI"]
-#	GET = env_vars["GET_URI"]
-	_send_http_request(GENERATE)
-	_send_http_request(GET)
+	add_child(http_generate)
+	add_child(http_get)
+	
+	http_generate.request_completed.connect(_on_generate_completed)
+	http_get.request_completed.connect(_on_get_completed)
 
-func _send_http_request(url: String) -> void:
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
+	_send_http_request(http_generate, GENERATE)
+	_send_http_request(http_get, GET)
+
+func _send_http_request(http_request: HTTPRequest, url: String) -> void:
 	var headers = [
 		"Accept: application/json",
 		"Accept-Encoding: identity"
 	]
-	http_request.set_meta("endpoint", url)  # Store the endpoint as metadata
-	http_request.request_completed.connect(self._http_request_completed)
-
-	var error = http_request.request(url,headers) # Send the request
+	var error = http_request.request(url, headers)
 	if error != OK:
 		Logger.log("An error occurred in the HTTP request.", "Error", "DATA_GENERATOR")
 
 # Called when the HTTP request is completed.
-func _http_request_completed(result, response_code, _headers, body):
-	var endpoint: String
- 	# this is some awful code, god forgive me
-	if flipflop == 1:
-		endpoint = get_child(2).get_meta("endpoint")
-	elif flipflop == -1:
-		endpoint = get_child(1).get_meta("endpoint")
-	flipflop = flipflop * -1
-
-	if result != HTTPRequest.RESULT_SUCCESS:
-		Logger.log("Error connecting to data generator server", "Error", "DATA_GENERATOR")
-		return
-	if response_code != 200:
-		Logger.log("Error: Server returned an unexpected status code %d" % response_code, "Error", "DATA_GENERATOR")
+func _on_generate_completed(result, response_code, _headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		Logger.log("Error in generate request", "Error", "DATA_GENERATOR")
 		return
 
-	var parsedResult = JSON.parse_string(body.get_string_from_utf8())
-	if parsedResult is Dictionary:
-		if endpoint == GENERATE:
-			result_from_generate = parsedResult
-		elif endpoint == GET:
-			result_from_get = parsedResult
-		emit_signal("result_ready", endpoint)
+	var parsed = JSON.parse_string(body.get_string_from_utf8())
+	if parsed is Dictionary:
+		result_from_generate = parsed
+		emit_signal("result_ready", GENERATE)
 	else:
-		#print("Error Reading File")
-		Logger.log("Error reading data generator response", "Error", "DATA_GENERATOR")
+		Logger.log("Error parsing generate response", "Error", "DATA_GENERATOR")
 
-func _on_update_timer_timeout() -> void:
-	_ready()
-	
-func load_env(path: String):
-	if not FileAccess.file_exists(path):
-		push_warning(".env file not found at " + path)
+func _on_get_completed(result, response_code, _headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		Logger.log("Error in get request", "Error", "DATA_GENERATOR")
 		return
-	
-	var file = FileAccess.open(path, FileAccess.READ)
-	while not file.eof_reached():
-		var line = file.get_line().strip_edges()
-		if line == "" or line.begins_with("#"): # skip comments/empty
-			continue
-		var parts = line.split("=", true, 1)
-		if parts.size() == 2:
-			env_vars[parts[0].strip_edges()] = parts[1].strip_edges()
+
+	var parsed = JSON.parse_string(body.get_string_from_utf8())
+	if parsed is Dictionary:
+		result_from_get = parsed
+		emit_signal("result_ready", GET)
+	else:
+		Logger.log("Error parsing get response", "Error", "DATA_GENERATOR")
