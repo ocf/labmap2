64444df65078cd868f3e82b55ef4a8d8a670b524
kinn-edendev <kinn.edendev@gmail.com>
2025-10-21 15:32:12 -0700

    fix: moved some computers around

--- frontend/labmap2_server/static/Labmap2.service.worker.js
+++ frontend/labmap2_server/static/Labmap2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757722211|349650646';
+const CACHE_VERSION = '1761085899|699718475';
 /** @type {string} */
 const CACHE_PREFIX = 'Labmap2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
