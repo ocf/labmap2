3a023fcea3a1da7d67be15b2d7fc53e2738e4d79
24apricots <michaelzls@berkeley.edu>
2026-02-18 22:18:31 -0800

    add nix devshell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.godotPackages_4_3.godot ];
+        };
+      });
+}
