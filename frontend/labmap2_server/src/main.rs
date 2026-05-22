2e51a0273f14f421b14525dd5d86ebb6b4d7b00e
24apricots <michaelzls@berkeley.edu>
2026-05-21 19:08:30 -0700

    labmap2_server: pre-compress static assets
    - updated actix-files: 0.6.10 supports .try_compressed()
    - bumped labmap2_server package version and edition
    - ran cargo update
    - brotli is intended to be the format of choice, with gzip as a fallback
    
    compression results for labmap2.wasm (currently the largest file):
       5.19 MiB labmap2.wasm.br
       7.63 MiB labmap2.wasm.gz
      33.74 MiB labmap2.wasm
    
    this should result in around 6.5x faster load times for brotli and
    around 4.5x faster load times for gzip.

--- frontend/labmap2_server/src/main.rs
+++ frontend/labmap2_server/src/main.rs
@@ -11,7 +11,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
-            .service(Files::new("/", "static/").use_last_modified(true))
+            // try to serve pre-compressed assets if requested and the files exist
+            .service(Files::new("/", "static/").use_last_modified(true).try_compressed())
             .route("/health", web::get().to(HttpResponse::Ok))
     })
     .bind("0.0.0.0:8080")? // Bind to localhost on port 8080
